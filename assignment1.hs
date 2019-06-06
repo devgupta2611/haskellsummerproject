0badd864ab8768f10913e352d7db0640bd3a4354
Devanshu Gupta <49458632+devgupta2611@users.noreply.github.com>
2019-06-07 01:02:16 +0530

    Update assignment1.hs

--- assignment1.hs
+++ assignment1.hs
@@ -1,8 +1,6 @@
 let mylast a = last a
 
-
 let myreverse a = reverse a
-
 
 let isPalindrome a = if a == reverse a then True
                      else False
@@ -15,9 +13,11 @@ let rotate a b = (drop b a) + (take b a)
 
 let insertAt x xs n = [ if y == n-1 then x else if y < n-1 then xs!!y else xs!!(y-1) | y<-[0..length(xs)] ]
 
+let combinations 0 = [[]]
+    combinations n xs = [ xs!!index : x | index <- [0..(length(xs)-1)] , x <- combinations (n-1) (drop(index +1) xs) ]
 
 let isPrime 1 = True
     isPrime 2 = False
     isPrime n = if length [ x | x <- [2 .. n-1] , n `mod` x == 0 ] == 0 then True else False
 
-let com n xs = f ( head(xs) , com(n-1) tail(xs) ) ++ com n tail(xs)
+
