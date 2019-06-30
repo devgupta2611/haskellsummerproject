a11f659ea0b38e4d4a1fd297c20492983f7dbfb9
Devanshu Gupta <49458632+devgupta2611@users.noreply.github.com>
2019-06-30 17:20:10 +0530

    solved

--- assignment1.hs
+++ assignment1.hs
@@ -12,6 +12,8 @@ rotate a b = (drop b a) ++ (take b a)
 
 insertAt x xs n = [ if y == n-1 then x else if y < n-1 then xs!!y else xs!!(y-1) | y <- [0..length(xs)] ] 
 
+
+combinations :: Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
 combinations n xs = [ xs!!index : x | index <- [0..(length(xs)-1)] , x <- combinations (n-1) (drop(index +1) xs) ]
 
